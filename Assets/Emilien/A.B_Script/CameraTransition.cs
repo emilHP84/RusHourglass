b6bec3c814710fd82cd28cf55dbfcc753c757fe4
emil <ehp84@sfr.fr>
2023-05-17 01:29:08 +0200

    push graph

---
+++ Assets/Emilien/A.B_Script/CameraTransition.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTransition : MonoBehaviour
+{
+    public GameObject cam;
+    
+    void Update() {
+    }
+}
