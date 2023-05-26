520ded749d1bbdf0c3e13ca8e5221263b3f69783
emil <ehp84@sfr.fr>
2023-05-26 12:41:52 +0200

    sauvegarde bugs réglé, amélioration générationProcédurale

--- Assets/Emilien/A.B_Script/DataScript/DataSave.cs
+++ Assets/Emilien/A.B_Script/DataScript/DataSave.cs
@@ -1,13 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using File = OpenCover.Framework.Model.File;
 
 public class DataSave : MonoBehaviour
 {
     public ScriptableObject inventoryPlayer;
-
+    
+    
     public void Awake() {
+        string filePathData = Application.persistentDataPath + "/CoinsData.json";
+        if (UnityEngine.Windows.File.Exists(filePathData) == false) {
+            Debug.Log("test");
+            SaveToJson();
+        }
+        
         LoadJsonSave();
     }
 
@@ -23,8 +32,8 @@ public class DataSave : MonoBehaviour
         string filePath = Application.persistentDataPath + "/CoinsData.json";
         string inventoryData = System.IO.File.ReadAllText(filePath);
 
-        JsonUtility.FromJsonOverwrite(inventoryData, inventoryPlayer);
-        //inventoryPlayer = JsonUtility.FromJson<InventoryPlayer>(inventoryData);
+        JsonUtility.FromJsonOverwrite(inventoryData, inventoryPlayer); // (pour scriptableObject)
+        //inventoryPlayer = JsonUtility.FromJson<InventoryPlayer>(inventoryData); (pour script basique)
         Debug.Log("Sauvegarde Chargé");
     }
 }
